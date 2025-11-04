ab43fafa1eaaafdde25936b1a803249fbadeed32
kipngenohaaron <kipngenohaaron@gmail.com>
2025-11-04 13:16:46 +0300

    models created

--- hostel_management/accounts/admin.py
+++ hostel_management/accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CustomUser, StudentProfile, StaffProfile
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+admin.site.register(CustomUser, UserAdmin)
+admin.site.register(StudentProfile)
+admin.site.register(StaffProfile)
