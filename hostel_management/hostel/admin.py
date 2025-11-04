c18adfc81706e2840a71c57e8337545445fea122
kipngenohaaron <kipngenohaaron@gmail.com>
2025-11-04 09:32:47 +0300

    files modified

--- hostel_management/hostel/admin.py
+++ hostel_management/hostel/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
+from .models import Room, Booking
 
-# Register your models here.
-from django.contrib import admin
-from .models import Profile
-
-admin.site.register(Profile)
+admin.site.register(Room)
+admin.site.register(Booking)
