f613c1b7da014ebd3e0933b1d0a95428ee1272f3
kipngenohaaron <kipngenohaaron@gmail.com>
2025-11-04 13:39:52 +0300

    homepage created

--- hostel_management/hostel/admin.py
+++ hostel_management/hostel/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Room, Booking
+from .models import Hostel, Room, Allocation
 
+admin.site.register(Hostel)
 admin.site.register(Room)
-admin.site.register(Booking)
+admin.site.register(Allocation)
--- hostel_management/hostel/views.py
+++ hostel_management/hostel/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+# hostel/views.py
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
