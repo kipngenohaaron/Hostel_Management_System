0251cb227fe63677a283284cae5122d4ce7a59ee
kipngenohaaron <kipngenohaaron@gmail.com>
2025-11-04 14:04:33 +0300

    models created

--- hostel_management/urls.py
+++ hostel_management/urls.py
@@ -1,7 +1,16 @@
+# from django.contrib import admin
+# from django.urls import path, include
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include('hostelel.urls')),  # link to your app
+# ]
 from django.contrib import admin
 from django.urls import path, include
+from hostel.views import home  # import the view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('hostelel.urls')),  # link to your app
+    path('', home, name='home'),       # homepage
+    path('accounts/', include('accounts.urls')),  # login/logout URLs
 ]
