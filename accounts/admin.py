353279b1cb52d7baa9b99cfc6fb819ba99feb335
kipngenohaaron <kipngenohaaron@gmail.com>
2025-11-05 18:22:06 +0300

    Updating the files

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,26 @@
+# from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
+# from .models import CustomUser
+
+# @admin.register(CustomUser)
+# class CustomUserAdmin(UserAdmin):
+#     list_display = ('username', 'email', 'first_name', 'last_name', 'user_type', 'is_staff')
+#     fieldsets = UserAdmin.fieldsets + (
+#         (None, {'fields': ('user_type',)}),
+#     )
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'user_type', 'is_staff')
+    model = CustomUser
+    list_display = ('id', 'username', 'email', 'is_staff', 'user_type')
+    list_filter = ('user_type', 'is_staff', 'is_superuser')
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('user_type',)}),
+        ('User Type', {'fields': ('user_type',)}),
     )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('User Type', {'fields': ('user_type',)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
