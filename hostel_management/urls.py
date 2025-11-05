c745768491de34cb12d7b9a4e4f76807262ff19b
kipngenohaaron <kipngenohaaron@gmail.com>
2025-11-05 17:54:30 +0300

    file modification

--- hostel_management/urls.py
+++ hostel_management/urls.py
@@ -10,13 +10,11 @@
 # ]
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import render
-
-def home_view(request):
-    return render(request, 'home.html')
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view, name='home'),
     path('accounts/', include('accounts.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
+
