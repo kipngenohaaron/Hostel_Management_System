3603d87a3b806a294aa6c259d52e347fe75621cc
kipngenohaaron <kipngenohaaron@gmail.com>
2025-11-05 17:51:47 +0300

    file modification

--- accounts/models.py
+++ accounts/models.py
@@ -1,19 +1,28 @@
+# from django.contrib.auth.models import AbstractUser
+# from django.db import models
+
+# class CustomUser(AbstractUser):
+#     USER_TYPES = (
+#         ('admin', 'Admin'),
+#         ('staff', 'Staff'),
+#         ('student', 'Student'),
+#     )
+#     user_type = models.CharField(max_length=10, choices=USER_TYPES, default='student')
+
+#     def is_student(self):
+#         return self.user_type == 'student'
+
+#     def is_staff_user(self):
+#         return self.user_type == 'staff'
+
+#     def is_admin_user(self):
+#         return self.user_type == 'admin'
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    USER_TYPES = (
-        ('admin', 'Admin'),
-        ('staff', 'Staff'),
-        ('student', 'Student'),
-    )
-    user_type = models.CharField(max_length=10, choices=USER_TYPES, default='student')
+    is_student = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
 
-    def is_student(self):
-        return self.user_type == 'student'
-
-    def is_staff_user(self):
-        return self.user_type == 'staff'
-
-    def is_admin_user(self):
-        return self.user_type == 'admin'
+    def __str__(self):
+        return self.username
